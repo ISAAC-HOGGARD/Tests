0016609c963909e4c08059f7bba3a829346aef04
ISAAC-HOGGARD <isaachoggard88@gmail.com>
2026-07-30 11:51:39 +1200

    Add files via upload

---
+++ js/CPS script.js
@@ -0,0 +1,76 @@
+let button = document.getElementById("btnCPS");
+let stopButton = document.getElementById("Btn2");
+let numInput = document.getElementById("numInput");
+let submitBtn = document.getElementById("submitBtn");
+let start = false;
+let clickTimeMs;
+let clickTimeS;
+let intervalId;
+let timeoutId;
+
+button.textContent = "start";
+button.style.display = "none";
+stopButton.style.display = "none";
+
+submitBtn.addEventListener("click", function() {
+    input = numInput.value;
+    if (input === "" || isNaN(input) || Number(input) <= 0){
+        alert("Please enter a positive number");
+        return;
+    }
+    clickTimeMs = (Number(numInput.value)) * 1000;
+    clickTimeS = Number(numInput.value);
+    console.log(`Click time is ${clickTimeS}`);
+    button.style.display = "revert";
+    stopButton.style.display = "revert";
+    numInput.value = "";
+})
+
+stopButton.addEventListener("click", function(){
+    start = false;
+    console.log("Is the clicker is off");
+    button.style.display = "revert";
+    button.textContent = "start";
+    clearInterval(intervalId);
+    clearTimeout(timeoutId);
+})
+
+let clicks;
+button.addEventListener("click", function(){
+    if (start === false){
+        start = true;
+        button.textContent = "click";
+        clicks = 0;
+        let startTime = Date.now();
+        submitBtn.style.display = "none";
+        numInput.style.display = "none";
+        let cpsTime = document.getElementById("timeInfo");
+        cpsTime.textContent = `Time left: ${clickTimeS}`;
+
+        intervalId = setInterval(function() {
+            let secondsLeft = Number(((clickTimeS -(Date.now() - startTime) / 1000)).toFixed(0));
+            cpsTime.textContent = `Time left: ${secondsLeft}`;
+        }, 1000);
+
+        timeoutId = setTimeout(function() {
+            start = false;
+            let endTime = Date.now();
+            clearInterval(intervalId);
+            let totalTime = (endTime-startTime) / 1000;
+            console.log(`clicks is ${clicks}, time is ${totalTime}`);
+            let cps = clicks/totalTime;
+            cps = Number(cps.toFixed(2));
+            console.log(`You got ${cps} CPS`);
+            submitBtn.style.display = "revert";
+            numInput.style.display = "revert";
+            console.log("The clicker is off");
+            let cpsDisplay = document.getElementById("cpsInfo");
+            cpsDisplay.textContent = `Your CPS was ${cps}`;
+            cpsTime.textContent = "";
+            button.style.display = "none";
+        }, clickTimeMs);
+    } else {
+        clicks = clicks +1;
+        //console.log(clicks)
+    }
+})
---
+++ js/Reaction script.js
@@ -0,0 +1,39 @@
+let button = document.getElementById("BtnReaction");
+let startTime;
+let clickable = false;
+let timeoutId;
+
+setTimeout(function() {
+    let popup = document.getElementById("pop");
+    popup.textContent = "";
+}, 3000);
+
+document.getElementById("Btn2").addEventListener("click", function() {
+    clickable = false;
+    console.log(`Clickable - ${clickable}`);
+    button.style.backgroundColor = "Red";
+    let randomInt = Math.random() + 2;
+
+    timeoutId = setTimeout(function() {
+        clickable = true;
+        console.log(`Clickable - ${clickable}`);
+        button.style.backgroundColor = "Green";
+        startTime = Date.now();
+    }, randomInt * 1000);
+});
+
+button.addEventListener("click", function() {
+    if (clickable === true) {
+        let endTime = Date.now();
+        let reactionTime = (endTime - startTime);
+        console.log(reactionTime);
+        let reaction = document.getElementById("reactionInfo");
+        reaction.textContent = `${reactionTime}ms`
+        clickable = false;
+        button.style.backgroundColor = "white";
+    } else {
+        alert("Click the button when it's green");
+        button.style.backgroundColor = "white";
+        clearTimeout(timeoutId);
+    }
+});
